c2fc79f1f15f5aeb2e552309dcafcd8d3b2e0860
Martin Gabelmann <martin@gabelmann.biz>
2017-06-04 10:51:37 +0200

    add freedesktop dependency from copycatkiller

--- menu.lua
+++ menu.lua
@@ -1,6 +1,7 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
+local freedesktop = require("freedesktop")
 
 -- confirm <question> to run <func> by clicking on the notification
 function askfor(question, func) 
@@ -29,37 +30,37 @@ for theme_dir in io.popen('ls -1 ' .. awesome.themes_path):lines() do --using po
     end
 end
 
-appmenu = {}
 
-mymainmenu = awful.menu({ 
-    items = { 
-        { "logout", function() askfor("Do you really want to logout?",  awesome.quit) end},
-        
-        { "poweroff", function() askfor(
-            "Do you really want to shutdown?",  
-            function() awful.spawn(terminal .. " -x systemctl poweroff -i") end
-            ) 
-            end 
-        },
+myawesomemenu = { 
+    { "logout", function() askfor("Do you really want to logout?",  awesome.quit) end},
+    
+    { "poweroff", function() askfor(
+        "Do you really want to shutdown?",  
+        function() awful.spawn(terminal .. " -x systemctl poweroff -i") end
+        ) 
+        end 
+    },
 
-        { "reboot", function() askfor(
-            "Do you really want to reboot?",  
-            function() awful.spawn(terminal .. " -x systemctl reboot -i") end
-            ) 
-            end 
-        },
+    { "reboot", function() askfor(
+        "Do you really want to reboot?",  
+        function() awful.spawn(terminal .. " -x systemctl reboot -i") end
+        ) 
+        end 
+    },
 
-        { "standby", function() askfor(
-            "Do you really want to suspend?",  
-            function() awful.spawn(terminal .. " -x systemctl suspend") end
-            ) 
-            end 
-        },
-        
-        { "lock screen", terminal .. " -x slock" },       
-        { "theme switch", themeswitch },
-        { "awesome", appmenu, theme.awesome_icon }
-    }
+    { "standby", function() askfor(
+        "Do you really want to suspend?",  
+        function() awful.spawn(terminal .. " -x systemctl suspend") end
+        ) 
+        end 
+    },
+    
+    { "lock screen", terminal .. " -x slock" },       
+    { "theme switch", themeswitch },
+}
+
+mymainmenu = freedesktop.menu.build({
+    before = {{ "Awesome", myawesomemenu, beautiful.awesome_icon }}
 })
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
